ac80b055601b0cd8c715bd9b65aad00268642778
Takuma Kira <takuma@kirakiraworx.com>
2024-03-17 13:25:11 +0900

    [fix]: Treat CORS correctly

--- src/app/lambda_function.py
+++ src/app/lambda_function.py
@@ -1,23 +1,46 @@
 import json
+import os
 import repository
 import transformers
 
 
 def lambda_handler(event, context):
-    if event.get('path') == '/data':
+    try:
+        if event.get('path') == '/data':
+            if 'headers' in event and 'Origin' in event['headers']:
+                origin = event['headers']['Origin']
+                allowed_origins = [os.environ['FRONTEND_ORIGIN_PROD'], os.environ['FRONTEND_ORIGIN_DEV']]
+                if origin not in allowed_origins:
+                    response = {
+                        "statusCode": 400,
+                        "headers": {
+                            "Content-Type": "application/json",
+                            "Access-Control-Allow-Origin": "*",  # Allow requests from any origin for error responses
+                        },
+                        "body": json.dumps({"error": "Invalid request origin"})
+                    }
+                    return response
+                allowed_origin = origin
+            else:
+                allowed_origin = os.environ['FRONTEND_ORIGIN_PROD']
+                return {
+                    'statusCode': 200,
+                    'headers': {
+                        'Access-Control-Allow-Origin': allowed_origin,
+                    },
+                    'body': json.dumps(transformers.bundle(repository.get_results(), 'url', ['job_title', 'job_location', 'scrape_date', 'count']))
+                }
+        else:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'message': 'Not found.'})
+            }
+    except Exception as e:
+        print(e)
         return {
-            'statusCode': 200,
-            'headers': {
-                # TODO: Control properly
-                'Access-Control-Allow-Origin': 'http://localhost:3000',
-            },
-            'body': json.dumps(transformers.bundle(repository.get_results(), 'url', ['job_title', 'job_location', 'scrape_date', 'count']))
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Internal server error.'})
         }
-
-    return {
-        'statusCode': 500,
-        'body': json.dumps({'message': 'Internal server error.'})
-    }
 
 
 if __name__ == '__main__':
