f228460a2382ef12669c7f31fcefca8a337223e6
Takuma Kira <takuma@kirakiraworx.com>
2024-03-17 13:41:08 +0900

    [fix]: Treat CORS correctly

--- src/app/lambda_function.py
+++ src/app/lambda_function.py
@@ -7,29 +7,35 @@ import transformers
 def lambda_handler(event, context):
     try:
         if event.get('path') == '/data':
+            allowed_origins = [os.environ['FRONTEND_ORIGIN_PROD'], os.environ['FRONTEND_ORIGIN_DEV']]
+
+            print(event)
+            if event:
+                print(event.get('headers'))
+            if event.get('headers'):
+                print(event.get('headers').get('Origin'))
+
             if 'headers' in event and 'Origin' in event['headers']:
                 origin = event['headers']['Origin']
-                allowed_origins = [os.environ['FRONTEND_ORIGIN_PROD'], os.environ['FRONTEND_ORIGIN_DEV']]
-                if origin not in allowed_origins:
-                    response = {
-                        "statusCode": 400,
-                        "headers": {
-                            "Content-Type": "application/json",
-                            "Access-Control-Allow-Origin": "*",  # Allow requests from any origin for error responses
-                        },
-                        "body": json.dumps({"error": "Invalid request origin"})
-                    }
-                    return response
-                allowed_origin = origin
-            else:
-                allowed_origin = os.environ['FRONTEND_ORIGIN_PROD']
-                return {
-                    'statusCode': 200,
-                    'headers': {
-                        'Access-Control-Allow-Origin': allowed_origin,
+            print(origin)
+            if origin == None or origin not in allowed_origins:
+                response = {
+                    "statusCode": 400,
+                    "headers": {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",  # Allow requests from any origin for error responses
                     },
-                    'body': json.dumps(transformers.bundle(repository.get_results(), 'url', ['job_title', 'job_location', 'scrape_date', 'count']))
+                    "body": json.dumps({"error": "Invalid request origin"})
                 }
+                return response
+            allowed_origin = origin
+            return {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Origin': allowed_origin,
+                },
+                'body': json.dumps(transformers.bundle(repository.get_results(), 'url', ['job_title', 'job_location', 'scrape_date', 'count']))
+            }
         else:
             return {
                 'statusCode': 404,
